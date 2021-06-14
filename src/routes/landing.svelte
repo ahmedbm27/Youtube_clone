fcc1dee81389ee46f099f17f737b07e1c3b267bb
unknown <contact.ahmedbm@gmail.com>
2021-06-14 20:53:07 +0100

    up

--- src/routes/landing.svelte
+++ src/routes/landing.svelte
@@ -3,6 +3,20 @@ import Video from '../components/videoLanding.svelte'
 import SideNav from "../components/sidenav.svelte"
 let videos =[1,2,3,4,5,6,7,8,9,10,11,12]
 export let opened;
+
+let channelID="UCsBjURrPoezykLs9EqgamOA"
+let apiKey="AIzaSyCbJJDz1ZH6J8XU21fqe6n4eIPj0Yw5zUs";
+let link= "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId="+channelID+"&type=video&key="+apiKey+"&order=date&maxResults=60"
+let go = async () =>{
+    const res = await fetch(link)
+	const json = await res.json()
+    
+	const result = JSON.stringify(json)
+    const parsed = JSON.parse(result)
+    console.log(parsed.items);
+}
+
+go();
 </script>
 
 <style>
@@ -33,6 +47,5 @@ export let opened;
         
         {/each}
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/g6BprD_IIl4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </div>
